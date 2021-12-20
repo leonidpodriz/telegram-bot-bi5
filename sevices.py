bfd4f3bd908b59af37774a4b5bd65fd702cdbc64
Leonid Podriz <leonidpodriz@gmail.com>
2021-12-20 19:47:37 +0200

    Update with lambda an regular functions

--- sevices.py
+++ sevices.py
@@ -13,3 +13,15 @@ def greet_names(names):
 
 print(greet_name("Leonid"))
 print(greet_names(["Leonid", 'Oleg']))
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+ages = [19, 45, 32, 34, 12, 10, 22, 88, 87]
+even_ages = list(filter(is_even, ages))
+odd_ages = list(filter(lambda number: number % 2 == 1, ages))
+
+print(even_ages)
+print(odd_ages)
