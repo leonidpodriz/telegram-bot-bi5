3982cac8d8e51354b68aa65b0adc2c0e26f26397
Leonid Podriz <leonidpodriz@gmail.com>
2021-12-20 19:24:48 +0200

    Add second function example

--- sevices.py
+++ sevices.py
@@ -2,4 +2,14 @@ def greet_name(name):
     return f'Hello, {name}!'
 
 
-print(greet_name('Leonid'))
+def greet_names(names):
+    result = ''
+
+    for name in names:
+        result += greet_name(name) + '\n'
+
+    return result
+
+
+print(greet_name("Leonid"))
+print(greet_names(["Leonid", 'Oleg']))
